097ce7754e34a59774c38daae3dc0e787605fad9
lukejones <47660116+BetaDraconis@users.noreply.github.com>
2019-10-30 13:46:15 +0000

    MTDSA-4250: Added audit detail model and controller implementation for TriggerCalculation endpoint.

--- app/v1/controllers/TriggerCalculationController.scala
+++ app/v1/controllers/TriggerCalculationController.scala
@@ -21,8 +21,9 @@ import play.api.libs.json.JsValue
 import play.api.mvc._
 import v1.connectors.httpparsers.StandardHttpParser.SuccessCode
 import v1.controllers.requestParsers.TriggerCalculationParser
-import v1.handling.{RequestDefn, RequestHandling}
+import v1.handling.{AuditHandling, RequestDefn, RequestHandling}
 import v1.hateoas.HateoasFactory
+import v1.models.audit.{AuditError, AuditResponse, TriggerCalculationAuditDetail}
 import v1.models.errors._
 import v1.models.hateoas.HateoasWrapper
 import v1.models.request.{TriggerCalculationRawData, TriggerCalculationRequest}
@@ -77,6 +78,23 @@ class TriggerCalculationController @Inject()(authService: EnrolmentsAuthService,
   def triggerCalculation(nino: String): Action[JsValue] = authorisedAction(nino).async(parse.json) { implicit request =>
     val rawData = TriggerCalculationRawData(nino, AnyContentAsJson(request.body))
 
-    doHandleRequest(rawData)
+    val auditHandling = AuditHandling(
+      "triggerASelfAssessmentTaxCalculation",
+      "trigger-a-self-assessment-tax-calculation",
+      successEventFactory = (correlationId: String, status: Int, response: Option[JsValue]) =>
+        TriggerCalculationAuditDetail(request.userDetails,
+          nino,
+          request.body,
+          correlationId,
+          AuditResponse(status, Right(response))),
+      failureEventFactory = (correlationId: String, status: Int, errors: Seq[AuditError]) =>
+        TriggerCalculationAuditDetail(request.userDetails,
+          nino,
+          request.body,
+          correlationId,
+          AuditResponse(status, Left(errors)))
+    )
+
+    doHandleRequest(rawData ,Some(auditHandling))
   }
 }
--- app/v1/models/audit/AuditResponse.scala
+++ app/v1/models/audit/AuditResponse.scala
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package v1.models.audit
 
 import play.api.libs.json.{ JsValue, Json, OWrites }
