75054d7f3b080eba5b3c761fc40621d19eb9ead7
lukejones <47660116+BetaDraconis@users.noreply.github.com>
2019-12-11 21:50:58 +0000

    MTDSA-4280: Added template tests for taxable income

---
+++ test/v1/models/response/getTaxableIncome/summary/CalculationSummarySpec.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.models.response.getTaxableIncome.summary
+
+import play.api.libs.json._
+import support.UnitSpec
+import v1.models.utils.JsonErrorValidators
+
+class CalculationSummarySpec extends UnitSpec with JsonErrorValidators {
+
+  case class Test(mandatory: String, optional: Option[String])
+  object Test {implicit val reads: Reads[Test] = Json.reads[Test]}
+
+  val testJson = Json.parse(
+    """
+      |{
+      |   "mandatory" : "value",
+      |   "optional" : "value"
+      |}
+      |""".stripMargin)
+
+  testJsonProperties[Test](testJson)(
+    mandatoryProperties = Seq(
+      "mandatory"
+    ),
+    optionalProperties = Seq(
+      "optional"
+    )
+  )
+}
