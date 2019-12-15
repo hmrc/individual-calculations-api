33e035054ba673baf44c39c9d37ce6e05684fe0f
lukejones <47660116+BetaDraconis@users.noreply.github.com>
2019-12-15 21:17:20 +0000

    MTDSA-4280: added tests from template for extra coverage

---
+++ test/definition/ApiDefinitionFactorySpec.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package definition
+
+import com.typesafe.config.ConfigFactory
+import definition.APIStatus.{ALPHA, BETA}
+import play.api.Configuration
+import support.UnitSpec
+import v1.mocks.{MockAppConfig, MockHttpClient}
+
+class ApiDefinitionFactorySpec extends UnitSpec {
+
+  class Test extends MockHttpClient with MockAppConfig {
+    val apiDefinitionFactory = new ApiDefinitionFactory(mockAppConfig)
+    MockedAppConfig.apiGatewayContext returns "api.gateway.context"
+  }
+
+  "buildAPIStatus" when {
+    "the 'apiStatus' parameter is present and valid" should {
+      "return the correct status" in new Test {
+        MockedAppConfig.apiStatus returns "BETA"
+        apiDefinitionFactory.buildAPIStatus("1.0") shouldBe BETA
+      }
+    }
+
+    "the 'apiStatus' parameter is present and invalid" should {
+      "default to alpha" in new Test {
+        MockedAppConfig.apiStatus returns "ALPHO"
+        apiDefinitionFactory.buildAPIStatus("1.0") shouldBe ALPHA
+      }
+    }
+  }
+
+  "buildWhiteListingAccess" when {
+    "the 'featureSwitch' parameter is not present" should {
+      "return None" in new Test {
+        MockedAppConfig.featureSwitch returns None
+        apiDefinitionFactory.buildWhiteListingAccess() shouldBe None
+      }
+    }
+
+    "the 'featureSwitch' parameter is present and white listing is enabled" should {
+      "return the correct Access object" in new Test {
+
+        MockedAppConfig.featureSwitch.returns(Some(Configuration(ConfigFactory.parseString(
+          """|white-list.enabled = true
+             |white-list.applicationIds = ["anId"]
+             |""".stripMargin))))
+
+        apiDefinitionFactory.buildWhiteListingAccess() shouldBe Some(Access("PRIVATE", Seq("anId")))
+      }
+    }
+
+    "the 'featureSwitch' parameter is present and white listing is not enabled" should {
+      "return None" in new Test {
+        MockedAppConfig.featureSwitch.returns(Some(Configuration(ConfigFactory.parseString("""|white-list.enabled = false""".stripMargin))))
+        apiDefinitionFactory.buildWhiteListingAccess() shouldBe None
+      }
+    }
+  }
+
+}
--- test/v1/mocks/MockAppConfig.scala
+++ test/v1/mocks/MockAppConfig.scala
@@ -30,6 +30,7 @@ trait MockAppConfig extends MockFactory {
     def mtdIdBaseUrl: CallHandler[String] = (mockAppConfig.mtdIdBaseUrl _: () => String).expects()
     def featureSwitch: CallHandler[Option[Configuration]] = (mockAppConfig.featureSwitch _: () => Option[Configuration]).expects()
     def apiGatewayContext: CallHandler[String]            = (mockAppConfig.apiGatewayContext _: () => String).expects()
+    def apiStatus: CallHandler[String] = (mockAppConfig.apiStatus: String => String).expects("1.0")
   }
 }
 
