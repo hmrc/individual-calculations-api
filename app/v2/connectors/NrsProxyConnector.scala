3890a89565f2610b02bcd4d2738fb9afd20fbbca
“ngangaNjiraini” <52919747+ngangaNjiraini@users.noreply.github.com>
2021-04-13 12:28:53 +0100

    MTDSA-8517 fixed test errors

--- app/v2/connectors/NrsProxyConnector.scala
+++ app/v2/connectors/NrsProxyConnector.scala
@@ -29,7 +29,7 @@ class NrsProxyConnector @Inject()(http: HttpClient,
                                   appConfig: AppConfig) {
 
   def submit[T](nino: String, body: CrystallisationRequestBody)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Unit] = {
-    implicit val readsEmpty: HttpReads[Unit] = (_: String, _: HttpResponse) => ()
+    implicit val readsEmpty: HttpReads[Unit] = (_: String, _: String, _: HttpResponse) => ()
 
     http.POST[CrystallisationRequestBody, Unit](s"${appConfig.mtdNrsProxyBaseUrl}/mtd-api-nrs-proxy/$nino/itsa-crystallisation", body)
   }
