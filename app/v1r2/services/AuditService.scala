d04b8905bb739aaf42b5269bece9785213bdee83
Taiwo-AB <63714619+Taiwo-AB@users.noreply.github.com>
2021-04-19 13:15:15 +0100

    [MTDSA-8724] Make log levels read from config + removal of logging of audit details (#255)

--- app/v1r2/services/AuditService.scala
+++ app/v1r2/services/AuditService.scala
@@ -45,8 +45,6 @@ class AuditService @Inject()(auditConnector: AuditConnector,
       detail = Json.toJson(event.detail),
       tags = eventTags
     )
-    logger.info(s"Audit event :- dataEvent.tags :: ${dataEvent.tags} --  auditSource:: ${dataEvent.auditSource}" +
-      s" --- detail :: ${dataEvent.detail}")
     auditConnector.sendExtendedEvent(dataEvent)
   }
 }
