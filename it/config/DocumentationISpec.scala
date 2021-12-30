9aec8744f7cacb27c32b6b2ac80fd27341f17bc9
Narendra Vijayarao <narendra.vijayarao@digital.hmrc.gov.uk>
2021-12-30 13:47:59 +0000

    Reverted RAML version

--- it/config/DocumentationISpec.scala
+++ it/config/DocumentationISpec.scala
@@ -75,7 +75,7 @@ class DocumentationISpec extends IntegrationBaseSpec {
     "return the documentation for v2" in {
       val response: WSResponse = await(buildRequest("/api/conf/2.0/application.raml").get())
       response.status shouldBe Status.OK
-      response.body[String] should startWith("#%RAML 2.0")
+      response.body[String] should startWith("#%RAML 1.0")
     }
   }
 
