ba6d7d1708d6a21f393f8787407d3dd4c2d8c6ec
mattstephens-hmrc <matthew.stephens@digital.hmrc.gov.uk>
2022-01-19 12:02:12 +0000

    Dependency upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -22,8 +22,8 @@ object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc"   %% "bootstrap-backend-play-28" % "5.12.0",
-    "uk.gov.hmrc"   %% "play-hmrc-api"     % "6.2.0-play-28",
+    "uk.gov.hmrc"   %% "bootstrap-backend-play-28" % "5.19.0",
+    "uk.gov.hmrc"   %% "play-hmrc-api"     % "6.4.0-play-28",
     "org.typelevel" %% "cats-core"         % "2.6.1",
     "com.chuusai"   %% "shapeless"         % "2.4.0-M1"
   )
