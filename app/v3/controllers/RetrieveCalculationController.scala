41fa4a4de32dc017220a0993cc654c2b1687ba95
james-work-account <31282758+james-work-account@users.noreply.github.com>
2022-04-05 17:50:59 +0100

    [MTDSA-11609] scaffold retrieve tax calc v3

---
+++ app/v3/controllers/RetrieveCalculationController.scala
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v3.controllers
+
+import play.api.mvc._
+import utils.IdGenerator
+import v3.connectors.httpparsers.StandardHttpParser.SuccessCode
+import v3.controllers.requestParsers.RetrieveCalculationParser
+import v3.handler.{RequestDefn, RequestHandler}
+import v3.hateoas.HateoasFactory
+import v3.models.errors._
+import v3.models.hateoas.HateoasWrapper
+import v3.models.request.{RetrieveCalculationRawData, RetrieveCalculationRequest}
+import v3.models.response.retrieveCalculation.{RetrieveCalculationHateoasData, RetrieveCalculationResponse}
+import v3.services.{AuditService, EnrolmentsAuthService, MtdIdLookupService, StandardService}
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+class RetrieveCalculationController @Inject()(authService: EnrolmentsAuthService,
+                                              lookupService: MtdIdLookupService,
+                                              retrieveCalculationParser: RetrieveCalculationParser,
+                                              service: StandardService,
+                                              hateoasFactory: HateoasFactory,
+                                              auditService: AuditService,
+                                              cc: ControllerComponents,
+                                              idGenerator: IdGenerator,
+)(implicit val ec: ExecutionContext)
+    extends StandardController[RetrieveCalculationRawData,
+                               RetrieveCalculationRequest,
+                               RetrieveCalculationResponse,
+                               HateoasWrapper[RetrieveCalculationResponse],
+                               AnyContent](authService, lookupService, retrieveCalculationParser, service, auditService, cc, idGenerator) {
+  controller =>
+
+  implicit val endpointLogContext: EndpointLogContext =
+    EndpointLogContext(
+      controllerName = "RetrieveCalculationController",
+      endpointName = "retrieveCalculation"
+    )
+
+  override val successCode: SuccessCode = SuccessCode(OK)
+
+  override def requestHandlerFor(
+      playRequest: Request[AnyContent],
+      req: RetrieveCalculationRequest): RequestHandler[RetrieveCalculationResponse, HateoasWrapper[RetrieveCalculationResponse]] = {
+    RequestHandler[RetrieveCalculationResponse](RequestDefn.Get(req.backendCalculationUri))
+      .mapErrors {
+        case "INVALID_TAXABLE_ENTITY_ID" => (BAD_REQUEST, NinoFormatError)
+        case "INVALID_CALCULATION_ID"    => (BAD_REQUEST, CalculationIdFormatError)
+        case "INVALID_CORRELATIONID"     => (INTERNAL_SERVER_ERROR, DownstreamError)
+        case "INVALID_CONSUMERID"        => (INTERNAL_SERVER_ERROR, DownstreamError)
+        case "NO_DATA_FOUND"             => (NOT_FOUND, NotFoundError)
+        case "SERVER_ERROR"              => (INTERNAL_SERVER_ERROR, DownstreamError)
+        case "SERVICE_UNAVAILABLE"       => (INTERNAL_SERVER_ERROR, DownstreamError)
+      }
+      .withRequestSuccessCode(OK)
+      .mapSuccessSimple(rawResponse =>
+        hateoasFactory.wrap(rawResponse, RetrieveCalculationHateoasData(req.nino.nino, req.taxYear, req.calculationId)))
+
+  }
+
+  def retrieveCalculation(nino: String, taxYear: String, calculationId: String): Action[AnyContent] =
+    authorisedAction(nino).async { implicit request =>
+      val rawData = RetrieveCalculationRawData(nino, taxYear, calculationId)
+
+      doHandleRequest(rawData)
+    }
+}
---
+++ it/v3/endpoints/RetrieveCalculationControllerISpec.scala
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v3.endpoints
+
+import com.github.tomakehurst.wiremock.stubbing.StubMapping
+import play.api.http.HeaderNames.ACCEPT
+import play.api.http.Status._
+import play.api.libs.json.Json
+import play.api.libs.ws.{ WSRequest, WSResponse }
+import support.V3IntegrationBaseSpec
+import v3.models.errors._
+import v3.stubs.{ AuditStub, AuthStub, BackendStub, MtdIdLookupStub }
+
+class RetrieveCalculationControllerISpec extends V3IntegrationBaseSpec {
+
+  private trait Test {
+
+    val nino: String          = "AA123456A"
+    val taxYear: String       = "2018-19"
+    val calculationId: String = "f2fb30e5-4ab6-4a29-b3c1-c7264259ff1c"
+
+    def uri: String = s"/$nino/self-assessment/$taxYear/$calculationId"
+
+    def backendUrl: String = s"/income-tax/view/calculations/liability/$nino/$calculationId"
+
+    def setupStubs(): StubMapping
+
+    def request: WSRequest = {
+
+      setupStubs()
+      buildRequest(uri)
+        .withHttpHeaders((ACCEPT, "application/vnd.hmrc.3.0+json"))
+    }
+  }
+
+  "Calling the retrieveCalculation endpoint" should {
+
+    "return a 200 status code" when {
+
+      val successBody = Json.parse(
+        """
+          |{
+          |  "metadata" : {
+          |    "field": ""
+          |  },
+          |  "inputs" : {
+          |    "field": ""
+          |  },
+          |  "calculation" : {
+          |    "field": ""
+          |  },
+          |  "messages" : {
+          |    "field": ""
+          |  }
+          |}
+        """.stripMargin
+      )
+
+      "a valid request is made" in new Test {
+        override def setupStubs(): StubMapping = {
+          AuditStub.audit()
+          AuthStub.authorised()
+          MtdIdLookupStub.ninoFound(nino)
+          BackendStub.onSuccess(BackendStub.GET, backendUrl, Map(), OK, successBody)
+        }
+
+        val response: WSResponse = await(request.get())
+
+        response.status shouldBe OK
+        response.header("Content-Type") shouldBe Some("application/json")
+        response.json shouldBe successBody
+      }
+    }
+
+    "return the correct error code" when {
+      def validationErrorTest(requestNino: String,
+                              requestTaxYear: String,
+                              requestCalculationId: String,
+                              expectedStatus: Int,
+                              expectedBody: MtdError): Unit = {
+        s"validation fails with ${expectedBody.code} error" in new Test {
+
+          override val nino: String          = requestNino
+          override val taxYear: String       = requestTaxYear
+          override val calculationId: String = requestCalculationId
+
+          override def setupStubs(): StubMapping = {
+            AuditStub.audit()
+            AuthStub.authorised()
+            MtdIdLookupStub.ninoFound(requestNino)
+          }
+
+          val response: WSResponse = await(request.get())
+          response.status shouldBe expectedStatus
+          response.json shouldBe Json.toJson(expectedBody)
+          response.header("Content-Type") shouldBe Some("application/json")
+        }
+      }
+
+      val input = Seq(
+        ("AA1123A", "2018-19", "f2fb30e5-4ab6-4a29-b3c1-c7264259ff1c", BAD_REQUEST, NinoFormatError),
+        ("AA123456A", "20177", "f2fb30e5-4ab6-4a29-b3c1-c7264259ff1c", BAD_REQUEST, TaxYearFormatError),
+        ("AA123456A", "2016-17", "f2fb30e5-4ab6-4a29-b3c1-c7264259ff1c", BAD_REQUEST, RuleTaxYearNotSupportedError),
+        ("AA123456A", "2020-22", "f2fb30e5-4ab6-4a29-b3c1-c7264259ff1c", BAD_REQUEST, RuleTaxYearRangeInvalidError),
+        ("AA123456A", "2017-18", "bad id", BAD_REQUEST, CalculationIdFormatError)
+      )
+
+      input.foreach(args => (validationErrorTest _).tupled(args))
+
+      "the backend returns a service error" when {
+
+        def errorBody(code: String): String =
+          s"""
+             |{
+             |  "code": "$code",
+             |  "message": "backend message"
+             |}
+           """.stripMargin
+
+        def serviceErrorTest(backendStatus: Int, backendCode: String, expectedStatus: Int, expectedBody: MtdError): Unit = {
+          s"backend returns an $backendCode error and status $backendStatus" in new Test {
+
+            override def setupStubs(): StubMapping = {
+              AuditStub.audit()
+              AuthStub.authorised()
+              MtdIdLookupStub.ninoFound(nino)
+              BackendStub.onError(BackendStub.GET, backendUrl, backendStatus, errorBody(backendCode))
+            }
+
+            val response: WSResponse = await(request.get())
+            response.status shouldBe expectedStatus
+            response.json shouldBe Json.toJson(expectedBody)
+            response.header("Content-Type") shouldBe Some("application/json")
+          }
+        }
+
+        val input = Seq(
+          (BAD_REQUEST, "INVALID_TAXABLE_ENTITY_ID", BAD_REQUEST, NinoFormatError),
+          (BAD_REQUEST, "INVALID_CALCULATION_ID", BAD_REQUEST, CalculationIdFormatError),
+          (BAD_REQUEST, "INVALID_CORRELATIONID", INTERNAL_SERVER_ERROR, DownstreamError),
+          (BAD_REQUEST, "INVALID_CONSUMERID", INTERNAL_SERVER_ERROR, DownstreamError),
+          (NOT_FOUND, "NO_DATA_FOUND", NOT_FOUND, NotFoundError),
+          (INTERNAL_SERVER_ERROR, "SERVER_ERROR", INTERNAL_SERVER_ERROR, DownstreamError),
+          (SERVICE_UNAVAILABLE, "SERVICE_UNAVAILABLE", INTERNAL_SERVER_ERROR, DownstreamError)
+        )
+
+        input.foreach(args => (serviceErrorTest _).tupled(args))
+      }
+    }
+  }
+}
