193858569d6bd480b8f3557905cd1d3e8577d98e
vfryer12 <72739659+vfryer12@users.noreply.github.com>
2022-04-07 09:07:02 +0100

    [MTDSA-11637] Models for Retrieve TaxCalc(03.9-03.12)

---
+++ app/v3/models/response/retrieveCalculation/calculation/studentLoans/PlanType.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v3.models.response.retrieveCalculation.calculation.studentLoans
+
+import play.api.libs.json.Format
+import utils.enums.Enums
+
+sealed trait PlanType
+
+object PlanType {
+
+  case object plan1 extends PlanType
+  case object plan2 extends PlanType
+  case object postgraduate extends PlanType
+  case object plan4 extends PlanType
+
+  implicit val formats: Format[PlanType] = Enums.format[PlanType]
+  }
+
+
+
+
+
