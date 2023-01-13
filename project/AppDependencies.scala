af21769132f1aea11c53cfae2d366bedf9f87be6
jeremystone <6453521+jeremystone@users.noreply.github.com>
2023-01-13 10:19:07 +0000

    MTDSA-13967 - RAML to OAS conversion for v3 endpoints
    - add common files
    - list endpoint
    - submit endpoint

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -24,7 +24,7 @@ object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % bootstrapPlayVersion,
+    "uk.gov.hmrc"                  %% "bootstrap-backend-play-28" % bootstrapPlayVersion,
     "org.typelevel"                %% "cats-core"                 % "2.8.0",
     "com.chuusai"                  %% "shapeless"                 % "2.4.0-M1",
     "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.14.1"
@@ -32,12 +32,14 @@ object AppDependencies {
 
   def test(scope: String = "test, it"): Seq[sbt.ModuleID] = Seq(
     "org.scalatest"          %% "scalatest"          % "3.2.14"            % scope,
-    "com.vladsch.flexmark"   % "flexmark-all"        % "0.62.2"            % scope,
+    "com.vladsch.flexmark"    % "flexmark-all"       % "0.62.2"            % scope,
     "org.scalacheck"         %% "scalacheck"         % "1.17.0"            % scope,
-    "org.scalamock"          %% "scalamock"          % "5.2.0"            % scope,
-    "org.pegdown"            % "pegdown"             % "1.6.0"             % scope,
+    "org.scalamock"          %% "scalamock"          % "5.2.0"             % scope,
+    "org.pegdown"             % "pegdown"            % "1.6.0"             % scope,
     "com.typesafe.play"      %% "play-test"          % PlayVersion.current % scope,
     "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0"             % scope,
-    "com.github.tomakehurst" % "wiremock-jre8"       % "2.35.0"           % scope
+    "com.github.tomakehurst"  % "wiremock-jre8"      % "2.35.0"            % scope,
+    "io.swagger.parser.v3"    % "swagger-parser-v3"  % "2.0.24"            % scope
   )
+
 }
