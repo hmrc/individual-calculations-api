199d0fbba506869ad3f365532d6ff93e888e3e86
Darshan Balasingam <112945711+darshanbala-hmrc@users.noreply.github.com>
2023-04-25 12:24:30 +0100

    moved nino validation spec

--- test/api/controllers/requestParsers/validators/validations/NinoValidationSpec.scala
+++ test/api/controllers/requestParsers/validators/validations/NinoValidationSpec.scala
@@ -14,9 +14,8 @@
  * limitations under the License.
  */
 
-package v3.controllers.requestParsers.validators.validations
+package api.controllers.requestParsers.validators.validations
 
-import api.controllers.requestParsers.validators.validations.NinoValidation
 import api.models.errors.NinoFormatError
 import support.UnitSpec
 
