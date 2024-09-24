6b68dbaf0a1a46dd26c5a33c5072ec8a00cf093f
Stu Raynor <737342+MadStu@users.noreply.github.com>
2024-09-24 13:14:13 +0100

    MTDSA-23287 Shared Code Update (#539)
    * NOJIRA Remove advanced-rate enum from def1
    
    * MTDSA-23287 Shared code update

--- app/shared/controllers/validators/resolvers/ResolveTaxYear.scala
+++ app/shared/controllers/validators/resolvers/ResolveTaxYear.scala
@@ -38,6 +38,16 @@ object ResolveTaxYear extends ResolverSupport {
     case _ => Invalid(List(TaxYearFormatError))
   }
 
+  def resolverWithCustomErrors(formatError: MtdError, rangeError: MtdError): Resolver[String, TaxYear] = {
+    case value @ taxYearFormat(start, end) =>
+      if (end.toInt - start.toInt == 1)
+        Valid(TaxYear.fromMtd(value))
+      else
+        Invalid(List(rangeError))
+
+    case _ => Invalid(List(formatError))
+  }
+
   def apply(value: String): Validated[Seq[MtdError], TaxYear] = resolver(value)
 
   def apply(value: Option[String]): Validated[Seq[MtdError], Option[TaxYear]] =
@@ -55,10 +65,15 @@ object ResolveTaxYear extends ResolverSupport {
 
 }
 
-case class ResolveTaxYearMinimum(minimumTaxYear: TaxYear, error: MtdError = RuleTaxYearNotSupportedError) extends ResolverSupport {
+case class ResolveTaxYearMinimum(
+    minimumTaxYear: TaxYear,
+    notSupportedError: MtdError = RuleTaxYearNotSupportedError,
+    formatError: MtdError = TaxYearFormatError,
+    rangeError: MtdError = RuleTaxYearRangeInvalidError
+) extends ResolverSupport {
 
   val resolver: Resolver[String, TaxYear] =
-    ResolveTaxYear.resolver thenValidate satisfiesMin(minimumTaxYear, error)
+    ResolveTaxYear.resolverWithCustomErrors(formatError, rangeError) thenValidate satisfiesMin(minimumTaxYear, notSupportedError)
 
   def apply(value: String): Validated[Seq[MtdError], TaxYear] = resolver(value)
 
