72953c75703f2e35918308ecddd4db28376dda94
Vedant-N421 <147396311+Vedant-N421@users.noreply.github.com>
2024-11-04 16:40:03 +0000

    MTDSA-26972: Fixes typos and filepaths.

--- test/v7/retrieveCalculation/def3/model/response/Def3_RetrieveCalculationResponseSpec.scala
+++ test/v7/retrieveCalculation/def3/model/response/Def3_RetrieveCalculationResponseSpec.scala
@@ -29,7 +29,7 @@ import v7.retrieveCalculation.models.response.Def3_RetrieveCalculationResponse
 
 class Def3_RetrieveCalculationResponseSpec extends UnitSpec with Def3_CalculationFixture with JsonErrorValidators with Inside {
 
-  "Def2_RetrieveCalculationResponse" must {
+  "Def3_RetrieveCalculationResponse" must {
     "allow conversion from downstream JSON to MTD JSON" when {
       "JSON contains every field" in {
         val model = calculationDownstreamJson.as[Def3_RetrieveCalculationResponse]
@@ -42,7 +42,7 @@ class Def3_RetrieveCalculationResponseSpec extends UnitSpec with Def3_Calculatio
     }
   }
 
-  "Def2_RetrieveCalculationResponse adjustFields" when {
+  "Def3_RetrieveCalculationResponse adjustFields" when {
     val fullResponse = calculationDownstreamJson.as[Def3_RetrieveCalculationResponse]
 
     val ignoredTaxYear = "ignoredTaxYear"
