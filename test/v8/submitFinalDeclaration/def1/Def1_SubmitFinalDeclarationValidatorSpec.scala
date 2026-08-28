d3d62f4e425e558eebae7cd6fd0003011f659a59
tom-w-lloyd-hmrc <320615954+tom-w-lloyd-hmrc@users.noreply.github.com>
2026-08-28 15:05:28 +0100

    MTDSA-36180: Added FE minimum tax year validation for Submit Final Declaration

--- test/v8/submitFinalDeclaration/def1/Def1_SubmitFinalDeclarationValidatorSpec.scala
+++ test/v8/submitFinalDeclaration/def1/Def1_SubmitFinalDeclarationValidatorSpec.scala
@@ -95,6 +95,17 @@ class Def1_SubmitFinalDeclarationValidatorSpec extends UnitSpec {
           ))
       }
     }
+
+    "return RuleTaxYearNotSupported error" when {
+      "a tax year older than the minimum allowed is supplied" in {
+        val result = validator(validNino, "2016-17", validCalculationId, validFinalDeclaration).validateAndWrapResult()
+        result.shouldBe(
+          Left(
+            ErrorWrapper(correlationId, RuleTaxYearNotSupportedError)
+          ))
+      }
+    }
+
     "return FormatCalculationTypeError error" when {
       "an invalid tax year is supplied" in {
         val result = validator(validNino, validTaxYear, validCalculationId, "notValid").validateAndWrapResult()
